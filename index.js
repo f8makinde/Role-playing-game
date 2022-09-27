7faa41b2a10d6fbbdb53175d4266fe07146effca
Faith Makinde <faithmakinde0@gmail.com>
2022-09-27 14:31:12 +0100

    rendering characters

--- index.js
+++ index.js
@@ -3,26 +3,31 @@ const hero = {
    name: "Wizard",
    avatar: "images/wizard.png",
    health: 60,
-   dice: 6
+   diceRoll: 6,
+   diceCount: 3
 }
  const monster = {
     elementId: "monster",
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    dice: 4
+    diceRoll: 4,
+    diceCount: 2
  }
 
-
-
-
   function renderChar(data){
-    const {elementId, name, avatar, health, dice} = data;
+    const {elementId, name, avatar, health, diceRoll, diceCount} = data;
+    let diceHtml = ""
+    for(let i = 0; i < diceCount; i++){
+      diceHtml += `<div class="dice">${diceRoll}</div>`;
+    }
     document.getElementById(elementId).innerHTML = `<div class="character-card">
         <h4 class="name">${name}</h4>
         <img class="avatar" src="${avatar}"/>
         <p class="health">health: <b>${health}</b></p>
-        <div class="dice-container"><div class="dice">${dice} </div></div>
+        <div class="dice-container">
+        ${diceHtml} 
+          </div>
     </div>`
   }
 
