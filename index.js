25533338e25877541281f5f9dc02abf8eb43df6c
Faith Makinde <faithmakinde0@gmail.com>
2022-09-27 13:50:45 +0100

    implementing js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+const hero = {
+   elementId: "hero",
+   name: "Wizard",
+   avatar: "images/wizard.png",
+   health: 60,
+   dice: 6
+}
+ const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    dice: 4
+ }
