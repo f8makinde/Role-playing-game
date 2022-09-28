8334ce228db715629f534ba9eaf7e00cb7a0c7ba
Faith Makinde <faithmakinde0@gmail.com>
2022-09-28 02:13:53 +0100

    using array constructor

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+function getDiceRollArray(diceCount) {
+   return new Array(diceCount).fill(0).map(function(){
+        return Math.floor(Math.random() * 6) + 1;
+    })
+}
+
+  
+ function getDiceHtml(diceCount){
+    return getDiceRollArray(diceCount).map(function(num){
+        return `<div class="dice">${num}</div>`
+    }).join("")
+}
+
 const hero = {
    elementId: "hero",
    name: "Wizard",
    avatar: "images/wizard.png",
    health: 60,
-   diceRoll: [3, 1, 4],
    diceCount: 3 
 }
  const monster = {
@@ -11,23 +23,14 @@ const hero = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: [2, 3],
+   
     diceCount: 2
  }
- function getDiceRollArray(diceCount){
-    const newDiceRolls = []
-    for(let i = 0; i < diceCount; i++){
-        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-    }
-    return newDiceRolls;
- }
-  getDiceRollArray(3)
+
   function renderChar(data){
     
-    const {elementId, name, avatar, health, diceRoll, diceCount} = data;
-    let diceHtml = diceRoll.map(function(num){
-        return `<div class="dice">${num}</div>`
-    }).join("")
+    const {elementId, name, avatar, health, diceCount} = data;
+    let diceHtml = getDiceHtml(diceCount)
     document.getElementById(elementId).innerHTML = `<div class="character-card">
         <h4 class="name">${name}</h4>
         <img class="avatar" src="${avatar}"/>
