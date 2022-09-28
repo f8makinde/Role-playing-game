23bdf284652fe0ed844b79abf4751e0296835767
Faith Makinde <faithmakinde0@gmail.com>
2022-09-28 01:32:24 +0100

    generate a random value for the dice

--- index.js
+++ index.js
@@ -3,30 +3,37 @@ const hero = {
    name: "Wizard",
    avatar: "images/wizard.png",
    health: 60,
-   diceRoll: 6,
-   diceCount: 3
+   diceRoll: [3, 1, 4],
+   diceCount: 3 
 }
  const monster = {
     elementId: "monster",
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: 4,
+    diceRoll: [2, 3],
     diceCount: 2
  }
-
+ function getDiceRollArray(diceCount){
+    const newDiceRolls = []
+    for(let i = 0; i < diceCount; i++){
+        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return newDiceRolls;
+ }
+  getDiceRollArray(3)
   function renderChar(data){
     
     const {elementId, name, avatar, health, diceRoll, diceCount} = data;
-    let diceHtml = ""
-    for(let i = 0; i < diceCount; i++){
-      diceHtml += `<div class="dice">${diceRoll}</div>`
-    }
+    let diceHtml = diceRoll.map(function(num){
+        return `<div class="dice">${num}</div>`
+    }).join("")
     document.getElementById(elementId).innerHTML = `<div class="character-card">
         <h4 class="name">${name}</h4>
         <img class="avatar" src="${avatar}"/>
         <p class="health">health: <b>${health}</b></p>
-        <div class="dice-container">${diceHtml}
+        <div class="dice-container">
+        ${diceHtml}
         </div>
     </div>`
   }
