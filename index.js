f521cfeb203d6642082ad19ab2173c4d715bf4d6
Faith Makinde <faithmakinde0@gmail.com>
2022-11-08 23:28:21 +0100

    tidy up all code

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 import { characterData } from "./data.js";
-import { getDiceRollArray } from "./utilis.js";
- function Character(data){
-     Object.assign(this, data)
-
-     this.getDiceHtml = function(diceCount){
-        return getDiceRollArray(diceCount).map(function(num){
-            return `<div class="dice">${num}</div>`
-        }).join("")
-    }
-    this.getCharacterHtml = function(){ 
-
-    const {elementId, name, avatar, health, diceCount} = this;
-    let diceHtml = this.getDiceHtml(diceCount)
-       return `<div class="character-card">
-        <h4 class="name">${name}</h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b>${health}</b></p>
-        <div class="dice-container">
-        ${diceHtml}
-        </div>
-    </div>` 
-
-    }
- }
+import Character from "./character.js";
 
  const wizard = new Character(characterData.hero)
  const orc= new Character(characterData.monster)
 
   function render(){
-    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
-    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+    document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+    document.getElementById("monster").innerHTML = orc.getCharacterHtml();
   }
   render()
