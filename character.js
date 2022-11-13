2fb7f347a61304eae12e4fc9bbbda2f35e926ad8
Faith Makinde <faithmakinde0@gmail.com>
2022-11-13 23:25:01 +0100

    adding new monster to the game

--- character.js
+++ character.js
@@ -1,50 +1,49 @@
-import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from "./utilis.js";
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
 
-
-function Character(data){
+function Character(data) {
     Object.assign(this, data)
     this.maxHealth = this.health
+
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
 
-    this.getDiceHtml = function(){
-         this.currentDiceScore = getDiceRollArray(this.diceCount)
-         this.diceArray = this.currentDiceScore.map(num => `<div class="dice">${num}</div>`).join(" ")
-   }
-   this.takeDamage = function (attackScoreArray){
-    const totalAttackScore = attackScoreArray.reduce((total, num) => {
-        return total + num;
-    })
-     this.health -= totalAttackScore
-     if(this.health <= 0){
-        this.dead = true;
-        this.health = 0;
-     }
+    this.getDiceHtml = function () {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceArray = this.currentDiceScore.map((num) =>
+            `<div class="dice">${num}</div>`).join("")
+    }
+
+    this.takeDamage = function (attackScoreArray) {
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
+        this.health -= totalAttackScore
+        if (this.health <= 0) {
+            this.dead = true
+            this.health = 0
+        }
+    }
+
+    this.getHealthBarHtml = function () {
+        const percent = getPercentage(this.health, this.maxHealth)
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
+                            style="width:${percent}%;">
+                    </div>
+                </div>`  
+    }
     
-   }
-   this.getHealthBarHtml = function(){
-    const percent = getPercentage(this.health, this.maxHealth)
-   return `<div class="health-bar-outer">
-    <div class="health-bar-inner ${percent < 25 ? "danger": ""} " 
-        style="width: ${percent}%"
-    </div>
-</div>`
-    console.log(percent)
-}
-   this.getCharacterHtml = function(){ 
-
-   const {elementId, name, avatar, health, diceCount, diceArray} = this;
-     const healthBar = this.getHealthBarHtml()
-      return `<div class="character-card">
-       <h4 class="name">${name}</h4>
-       <img class="avatar" src="${avatar}"/>
-       <p class="health">health: <b>${health}</b></p>
-       ${healthBar}
-       <div class="dice-container">
-       ${diceArray}
-       </div>
-   </div>` 
-
-   }
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this
+        const healthBar = this.getHealthBarHtml()
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
+                <div class="dice-container">
+                    ${this.diceArray}
+                </div>
+            </div>`
+    }
 }
 
-export default Character 
+export default Character
--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-import { characterData } from "./data.js";
-import Character from "./character.js";
+import characterData from './data.js'
+import Character from './Character.js'
 
 let monstersArray = ["orc", "demon", "goblin"]
+
 function getNewMonster() {
-  const nextMonsterData = characterData[monstersArray.shift()]
-  return nextMonsterData ? new Character(nextMonsterData) : {}
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
 }
-  function attack(){
+
+function attack() {
     wizard.getDiceHtml()
     monster.getDiceHtml()
-    wizard.takeDamage(wizard.currentDiceScore)
-    monster.takeDamage(monster.currentDiceScore)
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
     render()
-    if(wizard.dead || monster.dead){
-      endGame()
+    if(wizard.dead){
+        endGame()
     }
-  }
-    function endGame(){
-      const endMessage = wizard.health === 0 && monster.health === 0 ?
-      "No victors - all creatures are dead" :
-      wizard.health > 0 ? "The Wizard Wins" :
-      "The Orc is Victorious"
+    else if(monster.dead){
+        if(monstersArray.length > 0){
+            monster = getNewMonster()
+            render()
+        }
+        else{
+            endGame()
+        }
+    }
+}
 
-      const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-      document.body.innerHTML = 
-      `<div class="end-game">
-      <h2>Game Over</h2>
-      <h3>${endMessage}</h3>
-      <p class="end-emoji">${endEmoji}</p>
-  </div>` 
-    }
- const wizard = new Character(characterData.hero)
- let monster = getNewMonster()
-  function render(){
-    document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-    document.getElementById("monster").innerHTML = monster.getCharacterHtml();
-  }
-  document.getElementById("attack-button").addEventListener("click", attack)
-  render()
+function endGame() {
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The Orc is Victorious"
+
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    document.body.innerHTML = `
+        <div class="end-game">
+            <h2>Game Over</h2> 
+            <h3>${endMessage}</h3>
+            <p class="end-emoji">${endEmoji}</p>
+        </div>
+        `
+}
+
+document.getElementById("attack-button").addEventListener('click', attack)
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
+}
+
+const wizard = new Character(characterData.hero)
+let monster = getNewMonster()
+render()
