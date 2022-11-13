3050e73652b412354e95c02ddc2ec64b6e8a0727
Faith Makinde <faithmakinde0@gmail.com>
2022-11-13 23:09:02 +0100

    adding new monster to the game

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import { characterData } from "./data.js";
 import Character from "./character.js";
 
-
+let monstersArray = ["orc", "demon", "goblin"]
+function getNewMonster() {
+  const nextMonsterData = characterData[monstersArray.shift()]
+  return nextMonsterData ? new Character(nextMonsterData) : {}
+}
   function attack(){
     wizard.getDiceHtml()
-    orc.getDiceHtml()
+    monster.getDiceHtml()
     wizard.takeDamage(wizard.currentDiceScore)
-    orc.takeDamage(orc.currentDiceScore)
+    monster.takeDamage(monster.currentDiceScore)
     render()
-    if(wizard.dead || orc.dead){
+    if(wizard.dead || monster.dead){
       endGame()
     }
   }
     function endGame(){
-      const endMessage = wizard.health === 0 && orc.health === 0 ?
+      const endMessage = wizard.health === 0 && monster.health === 0 ?
       "No victors - all creatures are dead" :
       wizard.health > 0 ? "The Wizard Wins" :
       "The Orc is Victorious"
@@ -27,11 +31,10 @@ import Character from "./character.js";
   </div>` 
     }
  const wizard = new Character(characterData.hero)
- const orc= new Character(characterData.monster)
-
+ let monster = getNewMonster()
   function render(){
     document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-    document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+    document.getElementById("monster").innerHTML = monster.getCharacterHtml();
   }
   document.getElementById("attack-button").addEventListener("click", attack)
   render()
